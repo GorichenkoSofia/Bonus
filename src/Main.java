631da4654d9377b24d682c109230abe21c4b7f68
POP CODE <lolo4ka69@list.ru>
2023-01-02 19:52:40 +0300

    123

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int bill = 450;
+
+        int refill = 25000;
+
+        int bonus = 0;
+if (refill > 1000){
+    bonus = refill/100;
+
+}
+bill= bill+refill+bonus;
+
+
+System.out.println("Клиент пополнил счет на "+refill+ "."+ " Бонусов: "+bonus+". Остаток на счету: "+bill );
+
+    }
+}
